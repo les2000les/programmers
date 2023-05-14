005ea9c87ae1a36afb4ad27b0aadb217c7fc5ada
SSong <72508211+les2000les@users.noreply.github.com>
2023-05-14 11:00:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = 0
+    for i in my_string:
+        for j in range(1, 10):
+            if i == str(j):
+                answer = answer+j
+            else:
+                answer = answer
+    return answer
