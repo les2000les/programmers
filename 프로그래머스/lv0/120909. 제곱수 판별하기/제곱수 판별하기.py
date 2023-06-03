88c29b2dfe77d028455e82e0ef340c2b7567685d
SSong <72508211+les2000les@users.noreply.github.com>
2023-06-04 00:48:41 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.06 ms, Memory: 9.95 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if i*i == n:
+            answer = 1
+            break
+        else:
+            answer = 2
+    return answer
