e68bcf90951b37333eab958075cb2a57bd8d2e95
SSong <72508211+les2000les@users.noreply.github.com>
2023-06-04 00:49:40 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,12 @@
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        if i == '2':
+            answer = answer + '0'
+        elif i == '0':
+            answer = answer + '5'
+        elif i == '5':
+            answer = answer + '2'
+        else:
+            continue
+    return answer
